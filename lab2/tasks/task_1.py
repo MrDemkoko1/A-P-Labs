c56e539f14dc0353d31a226fd0da7d6e345b2657
Demian Skakun <demian.skakun@gmail.com>
2024-09-24 23:31:42 +0300

    1-st task done

---
+++ lab2/tasks/task_1.py
@@ -0,0 +1,37 @@
+import math as m
+
+# Task_1
+
+# 1.
+# Range: [3, 6]
+# Step: 0,3
+# Function-1-1: 1/(sin(1/x)+4)
+# Condition-1: x < 4
+# Function-2: 1/(x**2 + ln(x))
+# Умова-2: 4 <= x < 5
+# Ф-ція-3: tg((x-3)**3)
+# Умова-3: x >= 5
+
+step = 0.3
+
+def func1(x):
+    print('Function: 1/(sin(1/x)+4)')
+    return 1 / (m.sin(1/x) + 4)
+
+def func2(x):
+    print('Function: 1/(x**2 + ln(x))')
+    return 1 / (x**2 + m.log(x))
+
+def func3(x):
+    print('Function: tg((x-3)**3)')
+    return m.tan((x-3) ** 3)
+
+for i in range(11):
+    x = 3 + round(i * step, 1)
+    print(f'x = {x}')
+    if x <= 4:
+        print(func1(x))
+    elif x < 5:
+        print(func2(x))
+    else:
+        print(func3(x))
