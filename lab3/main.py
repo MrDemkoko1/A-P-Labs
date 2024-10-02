6e23de50c06b7fc29e1d1a1a78d42ea3bd574f5f
Demian Skakun <demian.skakun@gmail.com>
2024-10-02 10:21:35 +0300

    ...

--- lab3/main.py
+++ lab3/main.py
@@ -1,16 +1,24 @@
 example_set = {3, 1, -32, 5, 11, -4, 10, 9}
+example_list = [5, 18, -3, -2, 12, 8]
+example_tuple = (1, -3)
 
-def find_third_bigger_element(x):
+
+def find_third_biggest_element(x):
 
     ordered_list = list(x)
+
+    if len(x) < 3:
+        return 'there is no such number. Your array is too short'
 
     for element in range(len(ordered_list)):
         for next_element in range(element + 1, len(ordered_list)):
             if ordered_list[element] >= ordered_list[next_element]:
                 ordered_list[element], ordered_list[next_element] = ordered_list[next_element], ordered_list[element]
 
-    return(ordered_list[2])
+    return(ordered_list[-3])
 
-find_third_bigger_element(example_set)
+find_third_biggest_element(example_set)
 
-print(f'Third bigger number in your array {example_set} is: {find_third_bigger_element(example_set)}')
+print(f'Third biggest number in your array {example_set} is: {find_third_biggest_element(example_set)}')
+print(f'Third biggest number in your array {example_list} is: {find_third_biggest_element(example_list)}')
+print(f'Third biggest number in your array {example_tuple} is: {find_third_biggest_element(example_tuple)}')
