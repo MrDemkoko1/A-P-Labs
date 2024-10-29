1e95b834af444adeafc53d5f9eb9eef87a21b668
Demian Skakun <demian.skakun@gmail.com>
2024-10-29 12:56:01 +0200

    additional task has been done

--- lab4/main.py
+++ lab4/main.py
@@ -3,10 +3,11 @@ class Farm:
     pub_number = 100
     pub_string = 'Info'
 
-    def __init__(self, location, animals_qty, vent_power):
+    def __init__(self, location, animals_qty, vent_power, milk):
         self.__location = location
         self.__animals_qty = animals_qty
         self.__vent_power = vent_power
+        self.__amount_of_milk = milk
         print('Farm has been created')
 
     def __del__(self):
@@ -21,6 +22,14 @@ class Farm:
     def get_vent_power(self): 
         return self.__vent_power
 
+    def get_amount_of_milk(self):
+        return self.__amount_of_milk
+
+    def efficiency(self):
+        if self.__animals_qty > 0:
+            return self.__amount_of_milk / self.__animals_qty
+        return 0
+
     def __str__(self):
         return f"Location: {self.__location}, Animals: {self.__animals_qty}, Fan Power: {self.__vent_power}W"
 
@@ -29,12 +38,23 @@ class Farm:
 
 
 def main():
-    farm1 = Farm('Lviv', 30, 300)
-    farm2 = Farm('Drogobych', 50, 350)
-    farm3 = Farm('Ternopil', 123, 430)
+    farm1 = Farm('Lviv', 30, 300, 28)
+    farm2 = Farm('Drogobych', 50, 350, 38)
+    farm3 = Farm('Ternopil', 123, 430, 125)
 
-    print(farm1)
-    print(farm2)
-    print(farm3)
+    farms = [farm1, farm2, farm3]
+
+    highest_efficiency = 0
+    most_efficient_farm = farms[0]
+
+    for farm in farms:
+        if farm.efficiency() > highest_efficiency:
+            highest_efficiency = farm.efficiency()
+            most_efficient_farm = farm
+
+    print(f'Farm 1:\n {farm1}')
+    print(f'Farm 2:\n {farm2}')
+    print(f'Farm 3:\n {farm3}')
+    print(f'The most efficient farm is:\n {most_efficient_farm}\n with efficiency {highest_efficiency}')
 
 main()
