88be4f242d31c56209048c5aebc97c417fc8125b
Demian Skakun <demian.skakun@gmail.com>
2024-11-07 14:28:51 +0200

    i have finally managed to implement counting average pressure in the specifc region in the spesific day as a method of "WeatherCalendar" class.

--- lab5/main.py
+++ lab5/main.py
@@ -8,15 +8,15 @@ class WeatherType(Enum):
     SNOWY = 'Snowy'
 
 class Weather:
-    def __init__(self, day, city, region, country, temp, humidity, pressure, wind_speed, weather_type):
+    def __init__(self, day, city, region, country, temp, humidity, wind_speed, pressure, weather_type):
         self.__day = day
         self.__city = city
         self.__region = region 
         self.__country = country
         self.__temp = temp
         self.__humidity = humidity
-        self.__pressure = pressure
         self.__wind_speed = wind_speed
+        self.__pressure = pressure
         self.__weather_type = weather_type
 
     def get_day(self):
@@ -36,18 +36,15 @@ class Weather:
 
     def get_humidity(self):
         return self.__humidity
+   
+    def get_wind_speed(self):
+        return self.__wind_speed
     
     def get_pressure(self):
         return self.__pressure
 
-    def get_wind_speed(self):
-        return self.__wind_speed
-    
     def get_weather_type(self):
         return self.__weather_type
-    
-    def count_average_press_in_region(self):
-        pass
 
     def is_lviv_weather(self):
         if self.__humidity > 80 and self.__weather_type == WeatherType.RAINY:
@@ -70,6 +67,25 @@ class WeatherCalendar():
 
     def get_records(self):
         return self.__records
+
+    def find_average_pressure_for_region(self, day):
+        region_pressures = {}
+
+        for record in self.__records:
+            if record.get_day() == day:
+                region = record.get_region()
+
+                if region not in region_pressures:
+                    region_pressures[region] = []
+
+                region_pressures[region].append(record.get_pressure())
+
+        avg_pressures = {}
+
+        for region, pressures in region_pressures.items():
+            avg_pressures[region] = sum(pressures) / len(pressures)
+        
+        return avg_pressures 
 
 def find_max_temperature(weather_calendar, day):
     temps_for_specific_day = []
@@ -106,20 +122,21 @@ def print_weather_calendar(weather_calendar):
         print(f'\n{weather}')
 
 def main():
-    weather1 = Weather('Monday', 'Lviv', 'Ukraine', 13, 85, 24, WeatherType.RAINY)
-    weather2 = Weather('Monday', 'Stanislaviv', 'Ukraine', 15, 60, 10, WeatherType.SUNNY)
-    weather3 = Weather('Tuesday', 'Ternopil', 'Ukraine', 10, 50, 15, WeatherType.CLOUDY)
-    weather4 = Weather('Saturday', 'Lviv', 'Ukraine', 10, 89, 27, WeatherType.RAINY)
-    weather5 = Weather('Sunday', 'Kyiv', 'Ukraine', 12, 64, 10, WeatherType.FOGGY)
-    weather6 = Weather('Saturday', 'Luhansk', 'Ukraine', 9, 79, 23, WeatherType.SNOWY)
-    weather7 = Weather('Tuesday', 'Ternopil', 'Ukraine', 11, 80, 18, WeatherType.RAINY)
-    weather8 = Weather('Wednesday', 'Sudzha', 'Ukraine', 8, 100, 15, WeatherType.RAINY)
-    weather9 = Weather('Thursday', 'Xarkiv', 'Ukraine', 9, 45, 15, WeatherType.SUNNY)
-    weather10 = Weather('Thursday', 'Peremyszl', 'Poland', 14, 80, 15, WeatherType.RAINY)
-    weather11 = Weather('Friday', 'Zhytomyr', 'Ukraine', 10, 58, 15, WeatherType.CLOUDY)
+    weather1 = Weather('Monday', 'Lwiw', 'Lwiw', 'Ukraine', 13, 85, 24, 738, WeatherType.RAINY)
+    weather2 = Weather('Monday', 'Drohobycz', 'Lwiw', 'Ukraine', 15, 60, 10, 743, WeatherType.SUNNY)
+    weather3 = Weather('Tuesday', 'Stryj', 'Lwiw', 'Ukraine', 10, 50, 15, 729, WeatherType.CLOUDY)
+    weather4 = Weather('Saturday', 'Basiwka', 'Lwiw', 'Ukraine', 10, 89, 27, 717, WeatherType.RAINY)
+    weather5 = Weather('Sunday', 'Ternopil', 'Ternopil', 'Ukraine', 12, 64, 10, 770, WeatherType.FOGGY)
+    weather6 = Weather('Saturday', 'Strusiw', 'Ternopil', 'Ukraine', 9, 79, 23, 736, WeatherType.SNOWY)
+    weather7 = Weather('Tuesday', 'Terebowla', 'Ternopil', 'Ukraine', 11, 80, 18, 705, WeatherType.RAINY)
+    weather8 = Weather('Wednesday', 'Czortkiw', 'Ternopil', 'Ukraine', 8, 100, 15, 777, WeatherType.RAINY)
+    weather9 = Weather('Thursday', 'Stanisławiw', 'Stanisławiw', 'Ukraine', 9, 45, 15, 758, WeatherType.SUNNY)
+    weather10 = Weather('Thursday', 'Kołomyja', 'Stanisławiw', 'Poland', 14, 80, 15, 729, WeatherType.RAINY)
+    weather11 = Weather('Friday', 'Kalusz', 'Stanisławiw', 'Ukraine', 10, 58, 15, 756, WeatherType.CLOUDY)
+    weather12 = Weather('Friday', 'Jaremcze', 'Stanisławiw', 'Ukraine', 10, 58, 15, 718, WeatherType.CLOUDY)
 
     weathers = [weather1, weather2, weather3, weather4, weather5,
-                weather6, weather7, weather8, weather9, weather10, weather11]
+                weather6, weather7, weather8, weather9, weather10, weather11, weather12]
     
     ordered_weathers = sort_by_day(weathers)
     
